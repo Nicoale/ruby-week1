9702b75d0c958be48fc052459b26d60871e11e34
nicoale <nicoale8786@gmail.com>
2020-08-05 14:20:34 +0300

    tabs working

--- urbnmg/src/App.js
+++ urbnmg/src/App.js
@@ -1,36 +1,60 @@
 import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 import logo from "./urbn.png";
 import "./App.css";
-import Tabs from "./Tabs";
+import Faqs from "./faq";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
         <Tabs>
-          <div label="Home">
-            Hey there, <em>Plant lover</em>!
-          </div>
-          <div label="FAQs">
-            After 'while, <em>Crocodile</em>!
-          </div>
-          <div label="Let's Talk">
-            Nothing to see here, this tab is <em>extinct</em>!
-          </div>
+          <TabList>
+            <Tab>Home</Tab>
+            <Tab>FAQs</Tab>
+            <Tab>Let's Talk</Tab>
+          </TabList>
+
+          <TabPanel>
+            <h6>
+              Are you an aspiring plant parent? How do you suppose you'd like to
+              grow yours?
+              <br />
+              Whether just for your balcony or a larger farm I would love to
+              help you actualise this.
+              <br />
+              Theres no better satisfaction than that of growing your food from
+              scratch and knowing its free from harmful chemicals, <br />
+              as you will get just what you want from your OWN farm. <br />
+              For those who would only love to to get ready homegrown nutritious
+              vegetablesof the same quality, look no further because
+              <br />
+              URBN Microgreens & Plants has your back!
+            </h6>
+          </TabPanel>
+          <TabPanel>
+            <div>
+              <Faq data={data} styles={styles} config={config} />
+            </div>
+            <h6>Ask Me anything on here</h6>
+          </TabPanel>
+          <TabPanel>
+            <h3>
+              {" "}
+              <p>How may I be of help?</p>
+              <a
+                className="App-link"
+                href="https://wa.me/25472520499"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Chat!
+              </a>
+            </h3>
+          </TabPanel>
         </Tabs>
-        <p>
-           How May I be of help?
-        </p>
-        <a
-          className="App-link"
-          href="https://wa.me/25472520499"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Let's Talk!
-        </a>
       </header>
     </div>
   );
